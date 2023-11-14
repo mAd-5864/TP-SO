e12adb3b417792522a8c71f7d88432d6da868576
Pedro Amorim <pepas52@gmail.com>
2023-11-14 10:04:28 +0000

    update jogoUI

--- TP_SO_2324-main/src/jogoUI.c
+++ TP_SO_2324-main/src/jogoUI.c
@@ -1,67 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "jogoUI.h"
 
-int main(int argc, char* argv[], char* envp[])
-{
-  if(argc != 2)
-  {
-    printf("[ERRO]: Introduza o nome do jogador como argumento.\n");
-    exit(1);
-  }
-
-  char* playerName = argv[1];
-  printf("Nome do jogador: %s\n\n", playerName);
-
-  char* command = (char *) malloc(sizeof(char) * MAXLEN);
-  do
-  {
-    printf("Introduza um comando: ");
-    fgets(command, MAXLEN, stdin);
-    commands(command);
-  } while(strcmp(command, "exit"));
-}
-
-void commands(char* command)
-{
-  char* commandAux = strtok(command, " \n");
-  char *username, *msg;
-
-  printf("\nComando: [%s]\n", commandAux);
-  if(!strcmp(commandAux, "players"))
-  {
-    playersCommand();
-  }
-  else if(!strcmp(commandAux, "msg"))
-  {
-    username = strtok(NULL, " ");
-    msg = strtok(NULL, " ");
-    if(username == NULL || msg == NULL)
-      printf("[ERRO] Syntax: msg <username> <message>\n");
-    else
-    {
-      printf("username: %s\n", username);
-      printf("msg: %s\n", msg);
-      msgCommand(username, msg);
+int main(int argc, char* argv[], char* envp[]) {
+    if(argc != 2) {
+        printf("[ERRO]: Introduza o nome do jogador como argumento.\n");
+        exit(1);
     }
-  }
-  else if(!strcmp(commandAux, "exit"))
-  {
-    exitCommand();
-    strcpy(command, "exit");
-  }
-  else printf(("[ERRO]: Comando invalido.\n"));
+
+    char playerName[MAX_USERNAME_SIZE];
+    strncpy(playerName, argv[1], MAX_USERNAME_SIZE);
+    printf("Nome do jogador: %s\n\n", playerName);
+
+    char command[MAX_COMMAND_SIZE];
+    do {
+        printf("Introduza um comando: ");
+        fgets(command, MAX_COMMAND_SIZE, stdin);
+        validateCommands(command);
+    } while(strcmp(command, "exit"));
 }
 
-void playersCommand()
-{
-  printf("\nComando [players] nao implementado.\n");
+void validateCommands(char* command) {
+    char* commandAux = strtok(command, " \n");
+    char username[MAX_USERNAME_SIZE];
+    char msg[MAX_COMMAND_SIZE - MAX_USERNAME_SIZE - 2];
+
+    printf("\nComando: [%s]\n", commandAux);
+    if(!strcmp(commandAux, "players")) {
+        playersCommand();
+    } else if(!strcmp(commandAux, "msg")) {
+        username[0] = '\0';
+        msg[0] = '\0';
+        strcat(username, strtok(NULL, " "));
+        strcat(msg, strtok(NULL, "\n"));
+        if(username[0] == '\0' || msg[0] == '\0') {
+            printf("[ERRO] Syntax: msg <username> <message>\n");
+        } else {
+            printf("username: %s\n", username);
+            printf("msg: %s\n", msg);
+            msgCommand(username, msg);
+        }
+    } else if(!strcmp(commandAux, "exit")) {
+        exitCommand();
+        strcpy(command, "exit");
+    } else {
+        printf("[ERRO]: Comando invalido.\n");
+    }
 }
 
-void msgCommand(char *username, char* msg)
-{
-  printf("\nComando [msg] nao implementado.\n");
+void playersCommand() {
+    printf("\nComando [players] nao implementado.\n");
 }
 
-void exitCommand()
-{
-  printf("\nComando [exit] nao implementado.\n");
+void msgCommand(char *username, char* msg) {
+    printf("\nComando [msg] nao implementado.\n");
+}
+
+void exitCommand() {
+    printf("\nComando [exit] nao implementado.\n");
 }
--- TP_SO_2324-main/src/jogoUI.h
+++ TP_SO_2324-main/src/jogoUI.h
@@ -1,6 +1,24 @@
-#include "utils.h"
+#ifndef JOGOUI_H
+#define JOGOUI_H
 
-void commands(char* command);
+//Constantes
+#define MAX_USERNAME_SIZE 30
+#define MAX_COMMAND_SIZE 50
+
+// Defenir estruturas
+typedef struct {
+    char username[MAX_USERNAME_SIZE];
+    // Adicionar mais se necessário
+} UserCredentials;
+
+
+// Declarar funções
+void validateCommands(char* command);
 void playersCommand();
 void msgCommand(char *username, char* msg);
 void exitCommand();
+
+void getUserCredentials(UserCredentials* credentials);
+
+#endif
+
